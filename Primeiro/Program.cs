1393c31f2baf00bce3532f0cb40f538d5ea0cb29
Daniel Nunes <danielnunes17@gmail.com>
2022-01-13 15:49:02 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Primeiro
+{
+class Program
+    {
+        static void Main()
+        {
+            var a = "Olá ";
+            var b = "Enfermeira";
+            Console.WriteLine(a + b);
+              
+        }
+    }
+}
